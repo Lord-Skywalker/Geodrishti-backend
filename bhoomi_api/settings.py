e75795c0a7dbb11fa93a18b196aac02e31a3c6d8
Anubhav Deb <anubhavjan2004@gmail.com>
2026-03-27 19:26:34 +0530

    Updated ALLOWED_HOSTS for Render

--- bhoomi_api/settings.py
+++ bhoomi_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5@1+(a6c9@_)7_i2hom0*vjaxk!b0-rgw^2ey969p_yt@s3nv!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
